15e8c95c1da39f6e46102515c1622f3f73b5ec93
umgiwon <ugwzzang@daum.net>
2025-01-09 17:17:24 +0900

    test commit

---
+++ src/main/java/com/bootjpabase/carenation/api/sample/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.bootjpabase.carenation.api.sample.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sample/")
+public class SampleController {
+
+}
