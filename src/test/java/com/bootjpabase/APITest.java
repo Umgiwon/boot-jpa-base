63b74ae9948de10bf02413bad39f4eadb32d68ee
umgiwon <ugwzzang@daum.net>
2025-02-26 13:08:11 +0900

    # 수정

--- src/test/java/com/bootjpabase/APITest.java
+++ src/test/java/com/bootjpabase/APITest.java
@@ -7,7 +7,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,16 +23,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @Transactional
-//@SpringBootTest()
+@SpringBootTest()
 class APITest {
 
-//    @Autowired
+    @Autowired
     private MockMvc mockMvc;
 
-//    @Autowired
+    @Autowired
     private ObjectMapper objectMapper;
 
-//    @Autowired
+    @Autowired
     private CarRepository carRepository;
 
     @BeforeEach
