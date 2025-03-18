4bab73fe88c4b776f100d7650be2acb7bf4d78ba
umgiwon <ugwzzang@daum.net>
2025-03-18 12:54:31 +0900

    # 수정

--- src/main/java/com/bootjpabase/global/domain/dto/BaseResponse.java
+++ src/main/java/com/bootjpabase/global/domain/dto/BaseResponse.java
@@ -1,5 +1,6 @@
 package com.bootjpabase.global.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Schema(description = "Base Response")
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // 객체를 json 으로 직렬화할 때 null 값인 필드를 무시
 public class BaseResponse {
 
     @Schema(description = "TimeStamp")
