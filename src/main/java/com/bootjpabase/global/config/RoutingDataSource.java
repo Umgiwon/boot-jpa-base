34ab39f2bea3ef0336485999c852acde130ff88f
umgiwon <ugwzzang@gmail.com>
2026-01-06 10:12:17 +0900

    add: DB cluster 설정 추가

--- src/main/java/com/bootjpabase/global/config/RoutingDataSource.java
+++ src/main/java/com/bootjpabase/global/config/RoutingDataSource.java
@@ -1,13 +1,16 @@
 package com.bootjpabase.global.config;
 
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 /**
  * Transaction 여부에 따라 DB를 routing 해주는 클래스
  */
-//public class RoutingDataSource extends AbstractRoutingDataSource {
-//
-//    @Override
-//    protected Object determineCurrentLookupKey() {
-//        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
-//                ? "slave" : "master";
-//    }
-//}
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+                ? "slave" : "master";
+    }
+}
